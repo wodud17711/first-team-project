462bce7f625c4e00c10ee9ff07bdc7ba0f14b558
sia20015-stack <sia20015@gmail.com>
2026-06-01 17:38:20 +0900

    style: 반려견 프로필 카드 디자인 개선

--- frontend/src/pages/dogs/DogListPage.jsx
+++ frontend/src/pages/dogs/DogListPage.jsx
@@ -48,118 +48,147 @@ function DogListPage() {
   return (
     <div className="p-4">
 
-      {/* 상단 제목 + 프로필 추가 버튼*/}
+      {/* 상단 제목 */}
       <div className="flex justify-between items-center mb-4">
-        <h1 className="text-[28px] font-bold">반려견 프로필 목록</h1>
-
-        <button
-          onClick={() => navigate("/dog-profile-create")}
-          className="px-4 py-2 bg-brand-500 text-[#FFFFE3] text-[14px] font-bold rounded-xl"
-        >
-          프로필 추가
-        </button>
+        <h1 className="text-[28px] text-sky-800 font-bold">반려견 프로필 목록</h1>
       </div>
-      <div className='w-full h-[1px] bg-brand-400 mb-[30px]'/>
+      <div className='w-full h-[1px] bg-sky-400 mb-[30px]'/>
 
 
       <div className='flex flex-col gap-[20px]'>
         {/* 프로필 목록 */}
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
 
           {dogs.map((dog, index) => (
             <div
               key={dog.id}
               onClick={() => navigate("/dog-profile-detail", { state: dog })}
-              className="group relative flex bg-white rounded-xl shadow px-6 py-5 gap-2 cursor-pointer"
+              className="group relative flex h-full bg-white rounded-xl shadow p-6 gap-2 cursor-pointer"
             >
+              
 
-              {/* 강아지 이미지 */}
-              <div className="relative shrink-0">
-                <img
-                  src={dog.img}
-                  className="w-[180px] h-[230px] rounded-2xl object-cover"
-                />
+              <div className='flex flex-col'>
+                {/* 강아지 이미지 */}
+                <div className="relative shrink-0">
+                  <img
+                    src={dog.img}
+                    className="w-[400px] h-[230px] rounded-xl object-cover"
+                  />
 
-                {/* 첫 번째 강아지만 대표 강아지 표시 */}
-                {dog.isMain && (
-                <span
-                  className="
-                    absolute top-3 left-3
-                    px-2 py-1 rounded-full
-                    bg-white/90 backdrop-blur
-                    text-[11px] font-bold
-                  "
-                >
-                  ⭐ 대표 강아지
-                </span>
-              )}
-              </div>
+                  {/* 첫 번째 강아지만 대표 강아지 표시 */}
+                  {dog.isMain && (
+                  <span
+                    className="
+                      absolute top-3 left-3
+                      px-2 py-1 rounded-full
+                      bg-white/90 backdrop-blur
+                      text-[11px] font-bold
+                    "
+                  >
+                    ⭐ 대표 강아지
+                  </span>
+                )}
+                </div>
 
-              {/* 강아지 정보 */}
-              <div className='flex flex-col flex-1 pr-4'>
-                <div className='flex items-center gap-3 mb-2'>
-                  <p className="text-[24px] font-bold">{dog.name}</p>
+                {/* 강아지 정보 */}
+                <div className='flex flex-col flex-1'>
+                  <div className="flex items-center justify-center text-white bg-gray-700 rounded-lg gap-6 mt-6 mb-2 p-2">
+                    <div className="flex items-center gap-2">
+                      <p className="text-[14px]">이름:</p>
+                      <p className="text-[14px]">{dog.name}</p>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                      <p className="text-[14px]">견종:</p>
+                      <p className="text-[14px]">{dog.breed}</p>
+                    </div>
+                  </div>
+                  
+                  <div className="flex flex-col gap-2 p-4 mb-2 rounded-lg text-[14px] border border-gray-700">
+                    {/* 생년월일 */}
+                    <div className="flex items-center gap-3">
+                      <p className="w-[80px]">생년월일🎂</p>
+                      <div className="flex-1 border-b border-dashed border-gray-400 translate-y-[2px]" />
+                      <p>{dog.birth}</p>
+                    </div>
+
+                    {/* 성별 */}
+                    <div className="flex items-center gap-3">
+                      <p className="w-[80px]">성별🤍</p>
+                      <div className="flex-1 border-b border-dashed border-gray-400 translate-y-[2px]" />
+                      <p>
+                        {dog.gender}
+                        <span className={dog.gender === "여아" ? "text-pink-400" : "text-sky-400"}>
+                          {dog.gender === "여아" ? "🩷" : "🩵"}
+                        </span>{" "}
+                      </p>
+                    </div>
+
+                    {/* 체중 */}
+                    <div className="flex items-center gap-3">
+                      <p className="w-[80px]">체중🐾</p>
+                      <div className="flex-1 border-b border-dashed border-gray-400 translate-y-[2px]" />
+                      <p>{dog.weight}kg</p>
+                    </div>
+
+                    {/* 성향 태그 */}
+                    <div className="flex items-center gap-3">
+                      <p className="w-[80px]">성향🧩</p>
+                      <div className="flex-1 border-b border-dashed border-gray-400 translate-y-[2px]" />
+                      <div className="flex flex-wrap gap-1">
+                        {dog.tags.map((tag) => (
+                          <span
+                            key={tag}
+                            className="
+                              px-2 py-[2px]
+                              rounded-full
+                              bg-gray-100
+                              text-[11px]
+                            "
+                          >
+                            {tag}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+
+                  </div>
+
+                  {/* 프로필 등록일 */}
                   <div className='flex px-3 py-1
-                      bg-sky-100 rounded-full text-sky-700 text-[12px] font-bold'>
-                    프로필 등록일 · 2026/01/01
+                        bg-sky-100 rounded-lg text-sky-700 text-[12px] font-bold'>
+                      프로필 등록일 · 2026/01/01
                   </div>
                 </div>
-                
-                <div className="flex flex-col gap-1 text-[14px]">
-                  <p className='pb-1 border-b-[1px] border-brand-300'>🎂 {dog.birth}</p>
-                  <p className='pb-1 border-b-[1px] border-brand-300'>🐶 {dog.breed}</p>
-                  <p className='pb-1 border-b-[1px] border-brand-300'>
-                    <span
-                      className={
-                        dog.gender === "여아"
-                          ? "text-pink-400"
-                          : "text-sky-400"
-                      }
-                    >
-                      {dog.gender === "여아" ? "🩷" : "🩵"}
-                    </span>{" "}
-                    {dog.gender}
-                  </p>
-                  <p className='pb-1 border-b-[1px] border-brand-300'>🐾 {dog.weight}kg</p>
-                  <p className='pb-1 border-b-[1px] border-brand-300'>🚶 {dog.favorwalktime.join(", ")}</p>
-                </div>
 
-                {/* 성향 태그 */}
-                <div className="flex flex-wrap gap-1 mt-3">
-                  {dog.tags.map((tag) => (
-                    <span
-                      key={tag}
-                      className="
-                        px-2 py-[2px]
-                        rounded-full
-                        bg-gray-100
-                        text-[11px]
-                      "
-                    >
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-                
-              </div>
-              
-              {/* 우측 화살표 */}
-              <div className="
-                absolute right-5 top-1/2 -translate-y-1/2
-                text-[36px]
-                text-gray-300
-                group-hover:text-txtcolor-500
-              ">
-                ›
               </div>
 
             </div>
           ))}
+
+          {/* 반려견 프로필 추가 */}
+          <div
+            onClick={() => navigate("/dog-profile-create")}
+            className="
+              flex items-center justify-center
+              h-full
+              hover:bg-[#F0F0F0]
+              border-2 border-dashed border-gray-300
+              rounded-xl
+              cursor-pointer
+              transition
+              group
+            "
+          >
+            <div className="flex flex-col items-center text-gray-400 group-hover:text-gray-600 transition">
+              <div className="text-[48px] font-bold">+</div>
+              <p className="text-[14px] mt-2">반려견 프로필 추가</p>
+            </div>
+          </div>
         </div>
 
       </div>
 
-      
     </div>
   )
 }
